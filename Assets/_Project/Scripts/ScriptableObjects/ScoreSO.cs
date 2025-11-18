f9cda5fb26a00169ee884e59e21589c8b832b520
DJTJ9 <>
2025-11-18 22:51:10 +0100

    More UI Tests Added

---
+++ Assets/_Project/Scripts/ScriptableObjects/ScoreSO.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Bowling Ball", menuName = "Scriptable Objects/Bowling Ball", order = 1)]
+public class ScoreSO : ScriptableObject
+{
+    public int score;
+}
