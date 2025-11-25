ed38cc5a084cf62e328e0ca7130d25525e8ab2ff
DJTJ9 <>
2025-11-25 22:25:46 +0100

    Simple Game Loop added

--- Assets/_Project/Scripts/Bowling Settings/ScoreCalculator.cs
+++ Assets/_Project/Scripts/Bowling Settings/ScoreCalculator.cs
@@ -6,21 +6,17 @@ public class ScoreCalculator : MonoBehaviour
 {
     [SerializeField] private GameScoreSO scoreSO;
     
-    private bool hasFallen;
-    
     [Inject] private BallSpawner ballSpawner;
 
     private void OnTriggerEnter(Collider other) {
         if (scoreSO == null) return;
         
         scoreSO.Value += ballSpawner.BallSO.pointMultiplier;
-        hasFallen = true;
     }
 
     private void OnTriggerExit(Collider other) {
         if (scoreSO == null) return;
         
-        hasFallen = false;
         scoreSO.Value -= ballSpawner.BallSO.pointMultiplier;
     }
 }
--- Assets/_Project/Scripts/Systems/BowlingBattleGameManager.cs
+++ Assets/_Project/Scripts/Systems/BowlingBattleGameManager.cs
@@ -1,5 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using ImprovedTimers;
+using Sirenix.OdinInspector;
 using UnityEngine.Events;
 
 public class BowlingBattleGameManager : MonoBehaviour
@@ -7,23 +9,36 @@ public class BowlingBattleGameManager : MonoBehaviour
     [SerializeField] private UnityEvent onPreparationPhaseStart;
     [SerializeField] private UnityEvent onReleaseBall;
     [SerializeField] private UnityEvent onRoundEnd;
-    
+    [SerializeField] private UnityEvent onGameEnd;
+
     [SerializeField] private float preparationPhaseDuration = 10f;
     [SerializeField] private float roundDuration = 15f;
-    
+    [SerializeField] private int maxRounds = 3;
+
+    private int roundIndex = 1;
+
     private CountdownTimer preparationPhaseTimer;
     private CountdownTimer roundTimer;
-    
-    private void Start()
+
+    [FoldoutGroup("Round Settings", expanded: true)]
+
+private void Start()
     {
-        onPreparationPhaseStart.Invoke();
+        ResetRoundIndex();
         
         preparationPhaseTimer = new CountdownTimer(preparationPhaseDuration);
         preparationPhaseTimer.OnTimerStop += ReleaseBall;
         
         roundTimer = new CountdownTimer(roundDuration);
-        roundTimer.OnTimerStop += PrepareNextRound;
-        roundTimer.OnTimerStart += StartPreparationPhase;
+        roundTimer.OnTimerStop += EndRound;
+        
+        onPreparationPhaseStart.Invoke();
+    }
+
+    private void OnDisable()
+    {
+        preparationPhaseTimer.OnTimerStop -= ReleaseBall;
+        roundTimer.OnTimerStop -= EndRound;
     }
 
     public void StartPreparationPhase()
@@ -32,13 +47,31 @@ public class BowlingBattleGameManager : MonoBehaviour
         preparationPhaseTimer.Start();
     }
 
-    private void PrepareNextRound() => onRoundEnd.Invoke();
+    private void EndRound()
+    {
+        HandleRoundEnd();
+        
+        ++roundIndex;
+    } 
 
     private void ReleaseBall()
     {
         onReleaseBall.Invoke();
         roundTimer.Reset();
         roundTimer.Start();
-    } 
+    }
+
+    private void HandleRoundEnd()
+    {
+        if (roundIndex == maxRounds)
+        {
+            onGameEnd.Invoke();
+            return;
+        }
+        
+        onRoundEnd.Invoke();
+        StartPreparationPhase();
+    }
     
+    private void ResetRoundIndex() => roundIndex = 1;
 }
